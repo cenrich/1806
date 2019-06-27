7827b8131f817a94dea2716a291448f2abb57f20
cenrich <ceci_enrich@hotmail.com>
2019-06-27 19:50:54 -0300

    método cumpleaños 2

--- main.js
+++ main.js
@@ -3,16 +3,17 @@
 //aquí lo vamos a hacer con instanciamiento de datos. Esta función representa el modelo de todas las estudiantes
 //al pasarle los datos, debe crear una nueva estudiante con todas las características
 //espera unos parámetros y se los asigna a sí mismo como propiedades
-function student (first_name,last_name,birth_date) {
-    this.first_name=first_name //tu first_name es que se recibe por parámetro.
-    this.last_name=last_name //this es un elemento referencial, llama a la función en la que estoy.
-    this.birth_date=birth_date
-    this.greet = () =>console.log(`Hola, soy ${this.first_name}`) // métodos que hagan cosas con valores propios, no cosas externas
-    this.birthday_greeting = () => {
-        const nextBirthday = Math.abs (new Date().getMonth()-birth_date.getMonth())
-        console.log(`Mi próximo cumpleaños es en ${nextBirthday} meses`)
+class student {
+    constructor(first_name, last_name, birth_date) {
+        this.first_name = first_name; //tu first_name es que se recibe por parámetro.
+        this.last_name = last_name; //this es un elemento referencial, llama a la función en la que estoy.
+        this.birth_date = new Date (birth_date);
+        this.greet = () => console.log(`Hola, soy ${this.first_name}`); // métodos que hagan cosas con valores propios, no cosas externas
+        this.birthday_greeting = () => {
+            const nextBirthday = Math.abs(new Date().getMonth() - this.birth_date.getMonth());
+            console.log(`Mi próximo cumpleaños es en ${nextBirthday} meses`);
+        };
     }
-    
 }
 
 //defino cómo voy a agrupar a las estudiantes
@@ -25,9 +26,8 @@ let createStudent = () =>{
     const nameField=document.getElementById('first_name') //crea un nodo con un elemento que ya existe en el DOM
     const lastNameField=document.getElementById('last_name')
     const birthDateField=document.getElementById('birth_date')
-    // const BDasDate = 
     //armo un objeto según mi modelo con esta información
-    const newStudent = new student (nameField.value,lastNameField.value,new Date (birthDateField.value))
+    const newStudent = new student (nameField.value,lastNameField.value,birthDateField.value)
     //y guardo el objeto en la lista
     allStudents.push(newStudent) 
     console.log(allStudents)
